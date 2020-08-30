51e008458960240dcdb45645585713d7403854ac
Kent Shikama <kent@kentshikama.com>
2020-08-30 01:59:18 -0700

    Add an example event to the calendar with an unknown end

--- src/GridView.tsx
+++ src/GridView.tsx
@@ -8,24 +8,35 @@ import { format } from "date-fns";
 interface Props {
   members: PersonData[];
 }
-export default class GridView extends React.Component<Props> {
-  render() {
-    return (
-      <div>
-        <div className="pb3">(calendar will go here.)</div>
+export default function GridView(props: Props) {
+  const events = [
+    {
+      classNames: ["unknown-future"],
+      title: "Unknown end",
+      start: "2020-08-01",
+      end: "2020-08-01",
+      backgroundColor: "rgba(0, 0, 0, 0)",
+      borderColor: "rgba(0, 0, 0, 0)"
+    }
+  ];
 
-        {computeHouseHoldQuarantinePeriod(this.props.members).map(
-          (result: CalculationResult) => {
-            return (
-              <div className="p32">
-                {result.person.name} {" quarantined until: "}{" "}
-                {format(result.date, "MM/dd/yyyy")}
-              </div>
-            );
-          }
-        )}
-        {/*<FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />*/}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+      />
+      {computeHouseHoldQuarantinePeriod(props.members).map(
+        (result: CalculationResult) => {
+          return (
+            <div className="p32">
+              {result.person.name} {" quarantined until: "}{" "}
+              {format(result.date, "MM/dd/yyyy")}
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
 }
