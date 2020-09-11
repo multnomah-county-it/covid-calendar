949e6b58e9b7ee2b6eb21f9d6a584cd4eab61031
KentShikama <kent@kentshikama.com>
2020-09-11 09:08:44 +0000

    Deploying to gh-pages from  @ e6fe7196e3cfecef994140941ec8c74aefc8a5f9 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-calendar/precache-manifest.bdfbc9c11107ef7749c326f6c9427719.js"
+  "/covid-calendar/precache-manifest.c91fa89287c70d3e0ad1266b21ae5062.js"
 );
 
 self.addEventListener('message', (event) => {
